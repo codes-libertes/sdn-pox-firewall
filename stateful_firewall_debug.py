4a735b3c0948b80d4745fa208e80032f0cec1773
codes-libertes <47018797+codes-libertes@users.noreply.github.com>
2019-02-09 20:35:16 +0100

    Add files via upload

--- stateful_firewall_debug.py
+++ stateful_firewall_debug.py
@@ -5,6 +5,7 @@ from netaddr import IPNetwork, IPAddress
 import pox.openflow.libopenflow_01 as of
 import pox.lib.packet as pkt
 from pox.lib.addresses import IPAddr
+import re
 
 log = core.getLogger()
 	
@@ -12,7 +13,7 @@ log = core.getLogger()
 
 class Firewall(object):
 
-	def __init__(self,connection):
+	def __init__(self, connection):
 		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
   		"""
 		self.connection = variable locale que on a enregistre connection (var externe)sur la var self.connection 
@@ -20,54 +21,55 @@ class Firewall(object):
 		self.connection = connection 
 		self.flow_table = {}
 		self.inside_network = ["10.0.0.0/24"]
-		self.config_ARP_flow()
-		self.config_ICMP_flow()
-		self.config_TCP_flow()
+		self.config_openflow()
 		"""
 		add.Listeners= comme Ecouteur pour Appeler la fonction _handle_PacketIn.
 		"""
 		connection.addListeners(self)		
 		 
-	def config_ARP_flow(self):
+	def config_openflow(self):
+		"""
+		All incoming packet flows to Controller
+		"""
 		self.config_protocol_flow(pkt.arp.REQUEST,pkt.ethernet.ARP_TYPE,None,None,None,None,True)
 		self.config_protocol_flow(pkt.arp.REPLY,pkt.ethernet.ARP_TYPE,None,None,None,None,True)
-                  		
-	def config_ICMP_flow(self):
-		self.config_protocol_flow(pkt.ipv4.ICMP_PROTOCOL,pkt.ethernet.IP_TYPE,None,None,None,None,False)
+		self.config_protocol_flow(pkt.ipv4.ICMP_PROTOCOL,pkt.ethernet.IP_TYPE,None,None,None,None,True)
+		self.config_protocol_flow(pkt.ipv4.TCP_PROTOCOL,pkt.ethernet.IP_TYPE,None,None,None,None,True)
 
-	def config_TCP_flow(self):
-		global config
-		for rule in config:
-			rule[srcip] = clean_ip(rule[srcip])	
-			rule[dstip] = clean_ip(rule[dstip])	
-			if rule[srcip] != 'any':
-				nw_src = rule[srcip]
-			else:	
-				nw_src = None
-			if rule[dstip] != 'any':
-				nw_dst = rule[dstip]
-			else:	
-				nw_dst = None
-			if rule[srcport] != 'any':
-				tp_src = int(rule[srcport])
-			else:	
-				tp_src = None
-			if rule[dstport] != 'any':
-				tp_dst = int(rule[dstport])
-			else:	
-				tp_dst = None
-			self.config_protocol_flow(pkt.ipv4.TCP_PROTOCOL,pkt.ethernet.IP_TYPE,nw_src,nw_dst,tp_src,tp_dst,True)
+	def check_policy_TCP(self, nw_src, nw_dst, tp_dst=0):
+		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
+		"""
+		If all 3 parameters in l4_fw_rules, drop the packet; 
+		"""	
+		global l4_fw_rules	
+		key = (str(nw_src),str(nw_dst),tp_dst)
+		if key in l4_fw_rules:
+			print "Policy from (%s) to (%s:%d) found" % (str(nw_src),str(nw_dst),tp_dst)
+			return True
+		else:
+			print "No policy from (%s) to (%s:%d) found" % (str(nw_src),str(nw_dst),tp_dst)
+			return False
+			
 
-	def config_protocol_flow(self,nw_proto,dl_type,nw_src,nw_dst,tp_src,tp_dst,to_controller):
+	def check_policy_UDP(self):
+		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
+	
+	def check_policy_ARP(self):
+		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
+
+	def check_policy_ICMP(self):
+		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
+
+	def config_protocol_flow(self, nw_proto, dl_type, nw_src, nw_dst, tp_src, tp_dst, to_controller):
 		"""
         	Configurations for ARP/ICMP/TCP/UDP packet flows
         	"""
 		msg = of.ofp_flow_mod()
 		match = of.ofp_match()
-		match.nw_src = None
-		match.nw_dst = None
-		match.tp_src = None
-		match.tp_dst = None
+		match.nw_src = nw_src
+		match.nw_dst = nw_dst
+		match.tp_src = tp_src
+		match.tp_dst = tp_dst
 		match.nw_proto = nw_proto
 		# 0x0800 for IPv4, 0x0806 for ARP
 		match.dl_type = dl_type	
@@ -75,7 +77,11 @@ class Firewall(object):
 		msg.hard_timeout = 0
 		msg.soft_timeout = 0
 		msg.priority = 32768
-		# TCP/UDP packets will be sent to Controller that decodes TCP flags
+		"""
+		@ARGS:
+			True: directly go up to Controller
+			False:directly bypass controller and forward packets
+		"""
 		if to_controller:
 			action = of.ofp_action_output(port=of.OFPP_CONTROLLER)
 		else:
@@ -84,12 +90,9 @@ class Firewall(object):
 		msg.actions.append(action)
 		self.connection.send(msg)
 
-	def resend_packet(self,packet):
+	def resend_packet(self, packet):
 		ip_packet = packet.payload
 		tcp_packet = ip_packet.payload
-
-		fields = [str(ip_packet.srcip),str(tcp_packet.srcport),str(ip_packet.dstip),str(tcp_packet.dstport)]
-                print fields	
 					   
                 self.config_protocol_flow(pkt.ipv4.TCP_PROTOCOL,pkt.ethernet.IP_TYPE,ip_packet.srcip,ip_packet.dstip,tcp_packet.srcport,tcp_packet.dstport,False)
                 self.config_protocol_flow(pkt.ipv4.TCP_PROTOCOL,pkt.ethernet.IP_TYPE,ip_packet.dstip,ip_packet.srcip,tcp_packet.dstport,tcp_packet.srcport,False)
@@ -100,13 +103,18 @@ class Firewall(object):
 		self.connection.send(msg)
 
 
-	def act_like_firewall(self,packet,packet_in):
+	def act_like_firewall(self, packet, packet_in):
 		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
-		
-		#pox/lib/packet/ethernet.py
-		#35020:0x88cc:LLDP
-		#2048 :0x0800:IP
+		"""
+		pox/lib/packet/ethernet.py
+		35020 :0x88cc:LLDP
+		2048  :0x0800:IP
+		34525 :0x86DD:IPv4
+		"""
 		if packet.type == packet.LLDP_TYPE:
+			return
+		
+		if packet.type == packet.IPV6_TYPE:
 			return
 		
 		print "Packet Type:", packet.type	
@@ -121,8 +129,12 @@ class Firewall(object):
 		if packet.type == packet.IP_TYPE:
 			ip_packet = packet.payload
 			print "IP packet:", ip_packet.protocol
-			#pox/lib/packet/ipv4.py
-			#ICMP_PROTOCOL = 1:TCP_PROTOCOL  = 6:UDP_PROTOCOL  = 17
+			"""
+			pox/lib/packet/ipv4.py
+			ICMP_PROTOCOL = 1
+			TCP_PROTOCOL  = 6
+			UDP_PROTOCOL  = 17
+			"""
 			if ip_packet.protocol == ip_packet.TCP_PROTOCOL:
 				print "TCP Packet"
 				tracker = TCPConnTrack(self,packet)
@@ -137,12 +149,10 @@ class Firewall(object):
 			elif ip_packet.protocol == ip_packet.ICMP_PROTOCOL:
 				print "ICMP Packet"
 
-			self.resend_packet(packet)
-
 	"""
 	la fonction _handle_PacketIn va traiter les donnees de OpenFlow [ARP, ICMP, TCP,UDP, ...]
 	"""
-	def _handle_PacketIn(self,event):
+	def _handle_PacketIn(self, event):
 		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
 		packet = event.parsed
 		if not packet.parsed:
@@ -152,7 +162,7 @@ class Firewall(object):
 		packet_in = event.ofp
 		self.act_like_firewall(packet,packet_in)			
 		
-	def check_IPinside(self,ip):
+	def check_IPinside(self, ip):
         	"""
         	Check if the IP is from inside the network or not.
         	"""
@@ -165,9 +175,9 @@ class Firewall(object):
 
 class TCPConnTrack(object):
 
-	def __init__(self,cap_obj,pkt):
+	def __init__(self,fw_obj,pkt):
 		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
-		self.cap= cap_obj
+		self.fw= fw_obj
 		self.pkt = pkt
 		self.set_flags()
 
@@ -178,9 +188,9 @@ class TCPConnTrack(object):
 		ip_packet = self.pkt.payload	
 		tcp_packet = ip_packet.payload
 		decode = '{0:09b}'.format(tcp_packet.flags)
-		print "decode:",decode
-		print "seq:",tcp_packet.seq
-		print "ack:",tcp_packet.ack
+		#print "decode:",decode
+		#print "seq:",tcp_packet.seq
+		#print "ack:",tcp_packet.ack
 		self.FIN = decode[-1]
 		self.SYN = decode[-2]
 		self.RST = decode[-3]
@@ -192,54 +202,61 @@ class TCPConnTrack(object):
 		self.NS  = decode[-9]
 	
 		flags = [str(self.NS),str(self.CWR),str(self.ECE),str(self.URG),str(self.ACK),str(self.PSH),str(self.RST),str(self.SYN),str(self.FIN)]
-		print flags
+		#print flags
 
 	def track_network(self): 
         	"""
         	Tracks inside and outside network traffic
         	"""
-		if self.cap.check_IPinside(self.pkt.payload.srcip):
-			self.track_inside_network()
+		if self.fw.check_IPinside(self.pkt.payload.srcip):
+			allow_resend = self.track_inside_network()
 		else:
-			self.track_outside_network()
+			allow_resend = self.track_outside_network()
+
+		if True == allow_resend:
+			self.fw.resend_packet(self.pkt)
 
 	def track_inside_network(self): 
 		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
 		ip_packet = self.pkt.payload	
 		tcp_packet = ip_packet.payload
-		fields = [str(ip_packet.srcip),str(tcp_packet.srcport),str(ip_packet.dstip),str(tcp_packet.dstport)]
-		print fields
 		key = (ip_packet.srcip,tcp_packet.srcport,ip_packet.dstip,tcp_packet.dstport)
-		
-		print "key:",key
-		print "flow_table:",self.cap.flow_table
 	
-		if key in self.cap.flow_table:
-			print "key in flow_table:",self.cap.flow_table
-			if self.cap.flow_table[key][:2] == [1,"out"] and self.SYN and self.ACK:
-				self.cap.flow_table[key][0] += 1
-				self.cap.flow_table[key][1] = "in"
+		if True == self.fw.check_policy_TCP(ip_packet.srcip,ip_packet.dstip,tcp_packet.dstport):
+			print "This packet matched the rule and dropped !!"
+			return False
+
+		return True 
+
+		print "key:",key
+		print "flow_table:",self.fw.flow_table
+	
+		if key in self.fw.flow_table:
+			print "key in flow_table:",self.fw.flow_table
+			if self.fw.flow_table[key][:2] == [1,"out"] and self.SYN and self.ACK:
+				self.fw.flow_table[key][0] += 1
+				self.fw.flow_table[key][1] = "in"
 				print "TCP Packet SYN ACK from inside"
 				return True
-			elif self.cap.flow_table[key][:2] == [2,"out"] and self.ACK:
-				self.cap.flow_table[key][0] += 1
-				self.cap.flow_table[key][1] += "in"
+			elif self.fw.flow_table[key][:2] == [2,"out"] and self.ACK:
+				self.fw.flow_table[key][0] += 1
+				self.fw.flow_table[key][1] += "in"
 				print "TCP Packet ACK from inside"
 				return True
-			elif self.cap.flow_table[key][2] < 100:
-				self.cap.flow_table[key][2] += 1
+			elif self.fw.flow_table[key][2] < 100:
+				self.fw.flow_table[key][2] += 1
 				return True
-			elif self.cap.flow_table[key][2] >= 100:
-				del self.cap.flow_table[key][2]
+			elif self.fw.flow_table[key][2] >= 100:
+				del self.fw.flow_table[key][2]
 				print "DOS attacks detected"
 				return False
 				
 		else:
 			#First TCP packet
 			if self.SYN:
-				print "self.SYN:",self.cap.flow_table
+				print "self.SYN:",self.fw.flow_table
 				#[Numbers of packets, Traffic direction,DOS]
-				self.cap.flow_table[key] = [1,"in", 0]
+				self.fw.flow_table[key] = [1,"in", 0]
 				print "First TCP Packet SYN from inside"	
 				return True	
 	
@@ -247,48 +264,52 @@ class TCPConnTrack(object):
 		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
 		ip_packet = self.pkt.payload	
 		tcp_packet = ip_packet.payload
-		#fields = [str(ip_packet.srcip),str(tcp_packet.srcport),str(ip_packet.dstip),str(tcp_packet.dstport)]
-		#print fields
 		key = (ip_packet.srcip,tcp_packet.srcport,ip_packet.dstip,tcp_packet.dstport)
+		
+		if True == self.fw.check_policy_TCP(ip_packet.srcip,ip_packet.dstip,tcp_packet.dstport):
+			print "This packet matched the rule and dropped !!"
+			return False
+
+		return True
 
 		print "key:",key
-		print "flow_table:",self.cap.flow_table
+		print "flow_table:",self.fw.flow_table
 
-		if key in self.cap.flow_table:
-			print "key in flow_table:",self.cap.flow_table
-			if self.cap.flow_table[key][:2] == [1,"in"] and self.SYN and self.ACK:
-				self.cap.flow_table[key][0] += 1
-				self.cap.flow_table[key][1] = "out"
+		if key in self.fw.flow_table:
+			print "key in flow_table:",self.fw.flow_table
+			if self.fw.flow_table[key][:2] == [1,"in"] and self.SYN and self.ACK:
+				self.fw.flow_table[key][0] += 1
+				self.fw.flow_table[key][1] = "out"
 				print "TCP Packet SYN ACK from outside"
 				return True
-			elif self.cap.flow_table[key][:2] == [2,"in"] and self.ACK:
-				self.cap.flow_table[key][0] += 1
-				self.cap.flow_table[key][1] += "out"
+			elif self.fw.flow_table[key][:2] == [2,"in"] and self.ACK:
+				self.fw.flow_table[key][0] += 1
+				self.fw.flow_table[key][1] += "out"
 				print "TCP Packet ACK from outside"
 				return True
-			elif self.cap.flow_table[key][2] < 100: #It's a check to prevent from DOS
-				self.cap.flow_table[key][2] += 1
+			elif self.fw.flow_table[key][2] < 100: #It's a check to prevent from DOS
+				self.fw.flow_table[key][2] += 1
 				return True
-			elif self.cap.flow_table[key][2] >= 100:
-				del self.cap.flow_table[key][2]
+			elif self.fw.flow_table[key][2] >= 100:
+				del self.fw.flow_table[key][2]
 				print "DOS attacks detected"
 				return False
 				
 		else:
 			#First TCP packet
 			if self.SYN:
-				print "self.SYN:",self.cap.flow_table
+				print "self.SYN:",self.fw.flow_table
 				#[Numbers of packets, Traffic direction,DOS]
-				self.cap.flow_table[key] = [1,"out", 0]
+				self.fw.flow_table[key] = [1,"out", 0]
 				print "First TCP Packet SYN from outside"	
 				return True	
 
 
 class UDPConnTrack(object):
 
-	def __init__(self,cap_obj,pkt):
+	def __init__(self,fw_obj,pkt):
 		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
-		self.cap = cap_obj
+		self.fw = fw_obj
 		self.pkt = pkt
 	
 	def track_network(self): 
@@ -297,15 +318,15 @@ class UDPConnTrack(object):
 
 class ARPConnTrack(object):
 
-	def __init__(self,cap_obj,pkt):
+	def __init__(self,fw_obj,pkt):
 		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
-		self.cap = cap_obj
+		self.fw = fw_obj
 		self.pkt = pkt
 	
 	def track_network(self): 
 		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
-		self.cap.config_protocol_flow(pkt.arp.REQUEST,pkt.ethernet.ARP_TYPE,None,None,None,None,False)
-		self.cap.config_protocol_flow(pkt.arp.REPLY,pkt.ethernet.ARP_TYPE,None,None,None,None,False)
+		self.fw.config_protocol_flow(pkt.arp.REQUEST,pkt.ethernet.ARP_TYPE,None,None,None,None,False)
+		self.fw.config_protocol_flow(pkt.arp.REPLY,pkt.ethernet.ARP_TYPE,None,None,None,None,False)
 
 
 def clean_ip(cidrAddress):
@@ -315,26 +336,40 @@ def clean_ip(cidrAddress):
 	uintAddress = IPAddr(strAddress[0]).toUnsigned()
 	hostMask = 32-int(strAddress[1])
 	hostAddress = uintAddress & ((1<<hostMask) -1 )
-	print "uintAddress:",uintAddress
-	print "hostMask:",hostMask
-	print "hostAddress:",hostAddress
+	#print "uintAddress:",uintAddress
+	#print "hostMask:",hostMask
+	#print "hostAddress:",hostAddress
 	if (hostAddress == 0):
 		return cidrAddress
 	else:
 		return strAddress[0]
 
-def parse_config(configuration):
+def str_bool(policy):
+	"""
+	policy: many options 
+	"""
+	if str(policy).lower() in ("yes","y","true","t","allow","permit","1"):
+		print "str_bool False"
+		return False
+	if str(policy).lower() in ("no","n","false","f","block","deny","0"):
+		print "str_bool True"
+		return True
+
+def parse_config_l4(config_l4):
 	print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
-	"""
- 	<srcip> [ / <netmask> ] <srcport> <dstip> [ / <netmask> ] <dstport>
-	"""
-	global config	
-	fin = open(configuration)
+	global l4_fw_rules	
+	fin = open(config_l4)
 	for line in fin:
-		rule = line.split()
+		rule = re.sub(r'\s', '',line).split(',')
 		print "rule:",rule
 		if (len(rule) > 0):
-			config.append(rule)
+			nw_src = clean_ip(rule[srcip])	
+			nw_dst = clean_ip(rule[dstip])	
+			tp_dst = int(rule[dstport])
+			key = (nw_src,nw_dst,tp_dst)
+			if True == str_bool(rule[policy]):
+				l4_fw_rules[key] = True	
+
 
 def parse_configmac(config_mac):
 	print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
@@ -356,14 +391,14 @@ def parse_configmac(config_mac):
 		if (len(macrule) > 0):
 			configmac.append(macrule)	
 	
-def launch(configuration="", config_mac=""):
+def launch(config_l4="", config_mac=""):
 
 	print "Starting Pox Firewall.."
 	def start_firewall(event):
 		print ("[%s][%d][%s]" % (sys._getframe().f_code.co_filename,sys._getframe().f_lineno,sys._getframe().f_code.co_name))
 		Firewall(event.connection)
 
-	parse_config(configuration)
+	parse_config_l4(config_l4)
 	parse_configmac(config_mac)
 	"""
 	core pour connecter au pare-feu via la fonction en haut (firewall (eveent.connection)) et appliquer la class en haut class firewall (object)
@@ -371,12 +406,19 @@ def launch(configuration="", config_mac=""):
 	core.openflow.addListenerByName("ConnectionUp",start_firewall)
 """
 Global variables
+@l4_fw_rules: <srcip>,<dstip>,<dstport>,<port>
+	   <srcip>:
+	   <dstip>:
+	   <dstport>:
+	   <policy>:
+		-"yes","y","true","t","allow","permit","1"
+		-"no","n","false","f","block","deny","0"
 """
-config  = []
+l4_fw_rules  = {}
 srcip   = 0
-srcport = 1
-dstip   = 2
-dstport = 3
+dstip   = 1
+dstport = 2
+policy  = 3
 configmac  = []
 macsrc   = 0
 macdst = 1
